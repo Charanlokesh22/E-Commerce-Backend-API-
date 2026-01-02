14ceb921e1d01bfd8bc5ebfe1b885c9dce9281ca
Charanlokesh22 <charanlokesh2918@gmail.com>
2026-01-02 15:05:20 +0530

    Implement ProductViewSet with admin permissions

---
+++ products/views.py
@@ -0,0 +1,13 @@
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAdminUser
+from .models import Product
+from .serializers import ProductSerializer
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get_permissions(self):
+        if self.action in ["create", "update", "delete"]:
+            return [IsAdminUser()]
+        return []
